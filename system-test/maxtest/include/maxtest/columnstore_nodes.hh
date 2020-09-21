2082e7ebf4be991c466925bd457568ac1347e853
Timofey Turenko <timofey.turenko@mariadb.com>
2020-09-21 22:40:35 +0300

    add missing columnstore_nodes.hh

---
+++ system-test/maxtest/include/maxtest/columnstore_nodes.hh
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2016 MariaDB Corporation Ab
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2024-08-24
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2 or later of the General
+ * Public License.
+ */
+#pragma once
+
+/**
+ * @file columnstorex_nodes.h - work with Columnstor setup
+ *
+ */
+
+#include <cerrno>
+#include <string>
+#include <maxtest/mariadb_nodes.hh>
+#include <maxtest/nodes.hh>
+
+#define CLUSTRIX_DEPS_YUM "yum install -y bzip2 wget screen ntp ntpdate vim htop mdadm"
+#define WGET_CLUSTRIX     "wget http://files.clustrix.com/releases/software/clustrix-9.1.4.el7.tar.bz2"
+#define UNPACK_CLUSTRIX   "tar xvjf clustrix-9.1.4.el7.tar.bz2"
+#define INSTALL_CLUSTRIX  "cd clustrix-9.1.4.el7; sudo ./clxnode_install.py --yes --force"
+
+class Columnstore_nodes : public Mariadb_nodes
+{
+public:
+
+    Columnstore_nodes(const char* pref, const char* test_cwd, bool verbose, std::string network_config)
+        : Mariadb_nodes(pref, test_cwd, verbose, network_config)
+    {
+    }
+
+};
