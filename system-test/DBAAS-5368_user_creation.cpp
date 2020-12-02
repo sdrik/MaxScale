4d65de1a48f864409cb496f3f29a8487fb565afd
Timofey Turenko <timofey.turenko@mariadb.com>
2020-12-02 15:44:54 +0200

    imporove DBAAS-5368_user_creation test

--- system-test/DBAAS-5368_user_creation.cpp
+++ system-test/DBAAS-5368_user_creation.cpp
@@ -57,9 +57,16 @@ int main(int argc, char* argv[])
 
     Test->tprintf("Connecting to RWSplit %s\n", Test->maxscales->ip4(0));
     Test->maxscales->connect_rwsplit(0);
+    Test->try_query(Test->maxscales->conn_rwsplit[0], "CREATE USER 'creator'@'%%' identified by 'AaSs12345678^'", i);
+    Test->try_query(Test->maxscales->conn_rwsplit[0], "REVOKE SUPER ON *.* FROM 'creator'@'%%'", Test->maxscales->user_name.c_str());
+    Test->maxscales->close_rwsplit(0);
+    Test->maxscales->user_name = "creator";
+    Test->maxscales->password = "AaSs12345678^'";
+    Test->maxscales->connect_rwsplit(0);
+
 
     Test->tprintf("Revoke super from %s", Test->maxscales->user_name.c_str());
-    Test->try_query(Test->maxscales->conn_rwsplit[0], "REVOKE SUPER ON *.* FROM '%s'@'%%'", Test->maxscales->user_name.c_str());
+
 
     pthread_create(&thread, NULL, switch_thread, Test);
 
